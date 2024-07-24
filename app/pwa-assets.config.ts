44bf189cf9e68972ed94e4d6899cd09195ab7e2a
JJ Teoh <teohjjteoh@gmail.com>
2024-07-24 11:01:56 +0800

    add splashscreens

--- app/pwa-assets.config.ts
+++ app/pwa-assets.config.ts
@@ -12,6 +12,15 @@ export default defineConfig({
     padding: 0,
     resizeOptions: { background: "white", fit: "contain" },
     darkResizeOptions: { fit: "contain", background: "black" },
+    png: {
+      compressionLevel: 9,
+      quality: 60,
+    },
+    name: (landscape, size, dark) => {
+      return `apple-splash-${landscape ? "landscape" : "portrait"}-${
+        typeof dark === "boolean" ? (dark ? "dark-" : "light-") : ""
+      }${size.width}x${size.height}.png`;
+    },
   }),
   images: ["../public/logo.svg"],
 });
