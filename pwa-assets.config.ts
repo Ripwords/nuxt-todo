1eac8e3abb11eb709da85f711a9c02b143deb5dc
JJ Teoh <teohjjteoh@gmail.com>
2024-07-24 21:20:04 +0800

    add assets

---
+++ pwa-assets.config.ts
@@ -0,0 +1,26 @@
+import {
+  combinePresetAndAppleSplashScreens,
+  defineConfig,
+  minimal2023Preset,
+} from "@vite-pwa/assets-generator/config";
+
+export default defineConfig({
+  headLinkOptions: {
+    preset: "2023",
+  },
+  preset: combinePresetAndAppleSplashScreens(minimal2023Preset, {
+    padding: 0,
+    resizeOptions: { background: "white", fit: "contain" },
+    darkResizeOptions: { fit: "contain", background: "black" },
+    png: {
+      compressionLevel: 9,
+      quality: 60,
+    },
+    name: (landscape, size, dark) => {
+      return `apple-splash-${landscape ? "landscape" : "portrait"}-${
+        typeof dark === "boolean" ? (dark ? "dark-" : "light-") : ""
+      }${size.width}x${size.height}.png`;
+    },
+  }),
+  images: ["public/logo.svg"],
+});
